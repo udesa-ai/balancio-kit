4babc8c2f552d0ff56d457e2ac3d890f3d53e6f0
AgustinGrillo <agrillo@itba.edu.ar>
2021-07-30 16:42:03 -0300

    Automatized lite model conversion.

--- Code/Simulation/scripts/lite_converter.py
+++ Code/Simulation/scripts/lite_converter.py
@@ -1,13 +1,19 @@
 import tensorflow as tf
+import os
 
-folder_name = "test2"
+FOLDER_NAME = "A2C_best"
 
-converter = tf.lite.TFLiteConverter.from_keras_model_file("/home/agus/Documents/UdeSA/Balancio_V0/Code/Simulation/scripts/Models/" + folder_name + "/model.h5")
+path2folder = os.getcwd() + "/Models/my_models/" + FOLDER_NAME
+converter = tf.lite.TFLiteConverter.from_keras_model_file(path2folder + "/model.h5")
 converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
 tflite_model = converter.convert()
 
-# Save the model to disk
-open("/home/agus/Documents/UdeSA/Balancio_V0/Code/Simulation/scripts/Models/" + folder_name + "/Lite/model_quantized.tflite", "wb").write(tflite_model)
+if not os.path.exists(path2folder + "/Lite"):
+    os.makedirs(path2folder + "/Lite")
 
-## RUN IN TERMINAL:
+# Save the model to disk
+open(path2folder + "/Lite/model_quantized.tflite", "wb").write(tflite_model)
+
+# Run in terminal
 # xxd -i model_quantized.tflite > model_quantized.cc
+os.system("xxd -i " + path2folder + "/Lite/" + "model_quantized.tflite" + " > " + path2folder + "/Lite/" + "model_quantized.cc")
