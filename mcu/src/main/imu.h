985dc42d08d9060c3ad4bdd5a6288c5320750760
Agustin Grillo <agrillo@itba.edu.ar>
2022-04-07 14:03:42 -0300

    Simplify IMU driver.

--- mcu/src/main/imu.h
+++ mcu/src/main/imu.h
@@ -10,9 +10,19 @@
 #define SCL 27
 
 // Global function prototypes
+/**
+ * Inertial measurement unit (IMU) configuration and initialization.
+ */
 void imu_setup(void);
-void imu_setup_dmp(void);
-void getEulerAngles_dmp(float *angles);
+
+/**
+ * Get acceleration values in Y and Z axis, and angular velocity in X and Z axis.
+ *
+ * @param  {float*} ay : Pointer to store acceleration in Y axis.
+ * @param  {float*} az : Pointer to store acceleration in Z axis.
+ * @param  {float*} gx : Pointer to store angular velocity in X axis.
+ * @param  {float*} gz : Pointer to store angular velocity in Z axis.
+ */
 void getAccelGyro(float *ay, float *az, float *gx, float *gz);
 
 /**
@@ -26,7 +36,7 @@ float updatePitch(float currentAngle);
 
 /**
  * Estimate robot's yaw (in radians), integrating the gyroscope data.
- * 
+ *
  * @param  {float} currentYaw : Current yaw angle (in radians).
  * @return {float}            : New updated yaw angle (in radians).
  */
