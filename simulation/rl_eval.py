bd9a1c4b966f6e1bf9a5ac8157989e33ad561adc
AgustinGrillo <agrillo@itba.edu.ar>
2022-04-29 16:08:20 -0300

    Add name guard to RL training and evaluation scripts. Remove tensorflow deprecation warnings.

--- simulation/rl_eval.py
+++ simulation/rl_eval.py
@@ -1,24 +1,36 @@
+# ======================================================================
+#  Balancio-Kit (c) 2021 Linar (UdeSA)
+#  This code is licensed under MIT license (see LICENSE.txt for details)
+# ======================================================================
+"""
+Script for evaluating an RL agent on the Balancio.
+"""
+
+# Filter tensorflow version warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=Warning)
+import tensorflow as tf
+tf.get_logger().setLevel('INFO')
+tf.autograph.set_verbosity(0)
+import logging
+tf.get_logger().setLevel(logging.ERROR)
 
 from balancio_lib.environments import balancioGymEnv
-
-import tensorflow as tf
-# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-
 from stable_baselines import A2C
-
-import os
 import argparse
-
 
 # Instantiate the parser
 parser = argparse.ArgumentParser(description='Script to evaluate RL agents')
-parser.add_argument("-mn", "--modelName", action='store', default='model',  type=str, help="Name of the model to be evaluated [Default: model]")
+parser.add_argument("-mn", "--modelName", action='store', default='model', type=str,
+                    help="Name of the model to be evaluated [Default: model]")
 args = parser.parse_args()
 
-
 # Params
-CONVERT2KERAS = False    # Save Stable Baselines model to Keras, and evaluate it.
-MODEL_NAME = args.modelName     # Folder name where best_model.zip is held.
+CONVERT2KERAS = False  # Save Stable Baselines model to Keras, and evaluate it.
+MODEL_NAME = args.modelName  # Folder name where best_model.zip is held.
 NET_LAYERS = [32, 32]
 MEMORY_BUFFER = 1
 
@@ -26,46 +38,50 @@ MEMORY_BUFFER = 1
 NORMALIZE = True
 BACKLASH = True
 SEED = 5
+LoopFreq = 100  # Hz
+StepPeriod = (1 / 240) * 1 / 10  # s
+actions_per_step = int(round((1 / LoopFreq) / StepPeriod))  # For Microcontroller loop frequency compatibility
 
 # Directories
 training_save_path = os.path.join('../rl_data/models/')
 
 
-LoopFreq = 100  # Hz
-StepPeriod = (1/240) * 1/10  # s
-actions_per_step = int(round((1/LoopFreq)/StepPeriod))  # For Microcontroller loop frequency compatibility
+def main():
+    model = A2C.load(os.path.join(training_save_path, MODEL_NAME, 'best_model'))
+    env = balancioGymEnv.BalancioGymEnv(action_repeat=actions_per_step, renders=True, normalize=NORMALIZE,
+                                        backlash=BACKLASH, memory_buffer=MEMORY_BUFFER)
+
+    # Convert stable-baselines model to keras, for further lite conversion.
+    if CONVERT2KERAS:
+        keras_model = tf.keras.Sequential()
+        keras_model.add(tf.keras.layers.Dense(NET_LAYERS[0], input_dim=1))
+        keras_model.add(tf.keras.layers.Activation('relu'))
+        keras_model.add(tf.keras.layers.Dense(NET_LAYERS[1]))
+        keras_model.add(tf.keras.layers.Activation('relu'))
+        keras_model.add(tf.keras.layers.Dense(2))
+
+        params = model.get_parameters()
+        keras_model.layers[0].set_weights([params['model/shared_fc0/w:0'], params['model/shared_fc0/b:0']])
+        keras_model.layers[2].set_weights([params['model/shared_fc1/w:0'], params['model/shared_fc1/b:0']])
+        keras_model.layers[4].set_weights([params['model/pi/w:0'], params['model/pi/b:0']])
+
+        keras_model.save(os.path.join(training_save_path, MODEL_NAME, 'model.h5'))
+
+    while True:
+        obs = env.reset()
+        done = False
+        while done is False:
+            if CONVERT2KERAS:
+                action = keras_model.predict(obs)[0]
+                action.clip(-1, 1)
+                sb_action, _states = model.predict(obs, deterministic=True)
+                diff = action - sb_action
+                print("Diferencia entre predicciones: ", diff)
+            else:
+                action, _states = model.predict(obs, deterministic=True)
+            obs, reward, done, info = env.step(action)
+            env.render()
 
 
-model = A2C.load(os.path.join(training_save_path, MODEL_NAME, 'best_model'))
-env = balancioGymEnv.BalancioGymEnv(action_repeat=actions_per_step, renders=True, normalize=NORMALIZE, backlash=BACKLASH, memory_buffer=MEMORY_BUFFER)
-
-
-if CONVERT2KERAS:
-    keras_model = tf.keras.Sequential()
-    keras_model.add(tf.keras.layers.Dense(NET_LAYERS[0], input_dim=1))
-    keras_model.add(tf.keras.layers.Activation('relu'))
-    keras_model.add(tf.keras.layers.Dense(NET_LAYERS[1]))
-    keras_model.add(tf.keras.layers.Activation('relu'))
-    keras_model.add(tf.keras.layers.Dense(2))
-
-    params = model.get_parameters()
-    keras_model.layers[0].set_weights([params['model/shared_fc0/w:0'], params['model/shared_fc0/b:0']])
-    keras_model.layers[2].set_weights([params['model/shared_fc1/w:0'], params['model/shared_fc1/b:0']])
-    keras_model.layers[4].set_weights([params['model/pi/w:0'], params['model/pi/b:0']])
-
-    keras_model.save(os.path.join(training_save_path, MODEL_NAME, 'model.h5'))
-
-while True:
-    obs = env.reset()
-    done = False
-    while done is False:
-        if CONVERT2KERAS:
-            action = keras_model.predict(obs)[0]
-            action.clip(-1, 1)
-            sb_action, _states = model.predict(obs, deterministic=True)
-            diff = action - sb_action
-            print("Diferencia entre predicciones: ", diff)
-        else:
-            action, _states = model.predict(obs, deterministic=True)
-        obs, reward, done, info = env.step(action)
-        env.render()
+if __name__ == '__main__':
+    main()
--- simulation/rl_train.py
+++ simulation/rl_train.py
@@ -2,17 +2,30 @@
 #  Balancio-Kit (c) 2021 Linar (UdeSA)
 #  This code is licensed under MIT license (see LICENSE.txt for details)
 # ======================================================================
+"""
+Script for training an RL agent for the Balancio.
+"""
+
+# Filter tensorflow version warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=Warning)
+import tensorflow as tf
+tf.get_logger().setLevel('INFO')
+tf.autograph.set_verbosity(0)
+import logging
+tf.get_logger().setLevel(logging.ERROR)
 
 from balancio_lib.environments import balancioGymEnv
-import tensorflow as tf
-# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines import A2C
 from stable_baselines.common.callbacks import EvalCallback
 from stable_baselines.common import set_global_seeds
-import os
 import argparse
+from typing import Callable
 
 # Instantiate the parser
 parser = argparse.ArgumentParser(description='Script to train RL agent')
@@ -20,7 +33,7 @@ parser.add_argument("-mn", "--modelName", action='store', default='model', type=
                     help="Name where model and logs will be saved to [Default: model]")
 args = parser.parse_args()
 
-# Policy - hyperparams
+# Policy hyperparameters
 MODEL_NAME = args.modelName
 TIMESTEPS = 500000  # 1000000
 EVAL_FREQ = 5000
@@ -32,26 +45,18 @@ MEMORY_BUFFER = 1
 NORMALIZE = True
 BACKLASH = True
 SEED = 5
-
-# Directories
-training_save_path = os.path.join('../rl_data/models/', MODEL_NAME)
-training_log_path = os.path.join('../rl_data/logs/', MODEL_NAME)
-if not os.path.exists(training_save_path):
-    os.makedirs(training_save_path)
-if not os.path.exists(training_log_path):
-    os.makedirs(training_log_path)
-
 LoopFreq = 100  # Hz
 StepPeriod = (1 / 240) * 1 / 10  # s
 actions_per_step = int(round((1 / LoopFreq) / StepPeriod))  # For Microcontroller loop frequency compatibility
 
 
-def make_env(rank, seed=0):
+def make_env(rank: int, seed: int = 0) -> Callable:
     """
     Utility function for multiprocessed env.
 
-    :param seed: (int) the inital seed for RNG
-    :param rank: (int) index of the subprocess
+    @param rank: Index of the subprocess
+    @param seed: The initial seed for RNG
+    @return _init:
     """
 
     def _init():
@@ -64,35 +69,44 @@ def make_env(rank, seed=0):
     return _init
 
 
-# env = balancioGymEnv.BalancioGymEnv(renders=False, normalize=NORMALIZE, backlash=BACKLASH)
-# env = DummyVecEnv([lambda: env])
+def main():
+    """Main training script."""
+    # Directories
+    training_save_path = os.path.join('../rl_data/models/', MODEL_NAME)
+    training_log_path = os.path.join('../rl_data/logs/', MODEL_NAME)
+    if not os.path.exists(training_save_path):
+        os.makedirs(training_save_path)
+    if not os.path.exists(training_log_path):
+        os.makedirs(training_log_path)
 
-
-if __name__ == '__main__':
-
-    env = SubprocVecEnv([make_env(rank=i, seed=SEED) for i in range(NUM_CPU)])
+    # Wrapper of environments used for training.
+    train_env = SubprocVecEnv([make_env(rank=i, seed=SEED) for i in range(NUM_CPU)])
+    # Environment used for evaluation.
     eval_env = DummyVecEnv([lambda: balancioGymEnv.BalancioGymEnv(action_repeat=actions_per_step, renders=False,
                                                                   normalize=NORMALIZE, backlash=BACKLASH,
                                                                   memory_buffer=MEMORY_BUFFER)])
-
     eval_callback = EvalCallback(eval_env,
                                  eval_freq=EVAL_FREQ,
                                  best_model_save_path=training_save_path,
                                  verbose=1)
     policy_kwargs = dict(act_fun=tf.nn.relu, net_arch=NET_LAYERS)
     # model = PPO2(MlpPolicy, env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log=training_log_path)
-    model = A2C(MlpPolicy, env, gamma=0.9, n_steps=16, ent_coef=3.2155672659533806e-05, max_grad_norm=0.6,
-                vf_coef=0.4439617266032203,
-                learning_rate=0.004030614464204483, alpha=0.9, policy_kwargs=policy_kwargs, verbose=1,
-                tensorboard_log=training_log_path)
+    model = A2C(MlpPolicy, train_env, gamma=0.9, n_steps=16, ent_coef=3.2155672659533806e-05, max_grad_norm=0.6,
+                vf_coef=0.4439617266032203, learning_rate=0.004030614464204483, alpha=0.9,
+                policy_kwargs=policy_kwargs, verbose=1, tensorboard_log=training_log_path)
     model.learn(total_timesteps=TIMESTEPS, callback=eval_callback)
 
-    env = balancioGymEnv.BalancioGymEnv(action_repeat=actions_per_step, renders=True, normalize=NORMALIZE,
-                                        backlash=BACKLASH, memory_buffer=MEMORY_BUFFER)
+    # Trained agent testing.
+    test_env = balancioGymEnv.BalancioGymEnv(action_repeat=actions_per_step, renders=True, normalize=NORMALIZE,
+                                             backlash=BACKLASH, memory_buffer=MEMORY_BUFFER)
     while True:
-        obs = env.reset()
+        obs = test_env.reset()
         done = False
         while done is False:
-            action, _states = model.predict(obs, deterministic=True)  # deterministic false wtf(?)
-            obs, reward, done, info = env.step(action)
-            env.render()
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, done, info = test_env.step(action)
+            test_env.render()
+
+
+if __name__ == '__main__':
+    main()
