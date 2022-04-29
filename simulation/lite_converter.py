cbf3cab3815a76d98221c652e413fb94ebfc0213
AgustinGrillo <agrillo@itba.edu.ar>
2022-04-29 16:26:58 -0300

    Add license and name guard to model converter.

--- simulation/lite_converter.py
+++ simulation/lite_converter.py
@@ -1,20 +1,44 @@
-import tensorflow as tf
+# ======================================================================
+#  Balancio-Kit (c) 2021 Linar (UdeSA)
+#  This code is licensed under MIT license (see LICENSE.txt for details)
+# ======================================================================
+"""
+Script for converting a keras model into a C byte array.
+"""
+
+# Filter tensorflow warnings
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=Warning)
+import tensorflow as tf
+tf.get_logger().setLevel('INFO')
+tf.autograph.set_verbosity(0)
+import logging
+tf.get_logger().setLevel(logging.ERROR)
 
-FOLDER_NAME = "A2C"
+FOLDER_NAME = "test"
 
-path2folder = os.path.join('/', *os.path.dirname(os.path.realpath(__file__)).split('/')[:-1], "rl_data/models", FOLDER_NAME)
 
-converter = tf.lite.TFLiteConverter.from_keras_model_file(path2folder + "/model.h5")
-# converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
-tflite_model = converter.convert()
+def main():
+    path2folder = os.path.join("../rl_data/models", FOLDER_NAME)
 
-if not os.path.exists(path2folder + "/lite"):
-    os.makedirs(path2folder + "/lite")
+    converter = tf.lite.TFLiteConverter.from_keras_model_file(path2folder + "/model.h5")
+    # converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+    tflite_model = converter.convert()
 
-# Save the model to disk
-open(path2folder + "/lite/model_quantized.tflite", "wb").write(tflite_model)
+    if not os.path.exists(path2folder + "/lite"):
+        os.makedirs(path2folder + "/lite")
 
-# Run in terminal
-# xxd -i model_quantized.tflite > model_quantized.cc
-os.system("xxd -i " + path2folder + "/lite/" + "model_quantized.tflite" + " > " + path2folder + "/lite/" + "model_quantized.cc")
+    # Save the model to disk
+    open(path2folder + "/lite/model_quantized.tflite", "wb").write(tflite_model)
+
+    # Run in terminal
+    # xxd -i model_quantized.tflite > model_quantized.cc
+    os.system(
+        "xxd -i " + path2folder + "/lite/" + "model_quantized.tflite" + " > " + path2folder + "/lite/" + "model_quantized.cc")
+
+
+if __name__ == '__main__':
+    main()
